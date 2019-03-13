000c414cb16a13973ff94d40f99856c98edeff71
vermucht <vermucht@gmail.com>
2019-03-13 17:10:07 +0300

    Реализовать модели и связи для задание SQL хранилище машин. [#4744] 04:03:25

---
+++ src/main/java/carstore/model/annotations/Body.java
@@ -0,0 +1,150 @@
+package carstore.model.annotations;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+/**
+ * TCar body bean.
+ *
+ * @author Aleksei Sapozhnikov (vermucht@gmail.com)
+ * @version 0.1
+ * @since 0.1
+ */
+@Entity
+@Table(name = "body")
+public class Body {
+    /**
+     * Logger.
+     */
+    @SuppressWarnings("unused")
+    private static final Logger LOG = LogManager.getLogger(Body.class);
+
+    /**
+     * Unique id.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "body_id")
+    private int id;
+
+    /**
+     * Body type (sedan, track, SUV)
+     */
+    @Column(name = "body_type")
+    private String type;
+    /**
+     * Body color (black, white, red)
+     */
+    @Column(name = "color")
+    private String color;
+
+    /* * * * * * * * * * * * * * * * * *
+     * equals(), hashCode(), toString()
+     * * * * * * * * * * * * * * * * * */
+
+    /**
+     * Object.equals() method override.
+     *
+     * @param o Other object.
+     * @return <tt>true</tt> if this and given objects are equal, <tt>false</tt> if not.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Body body = (Body) o;
+        return id == body.id
+                && Objects.equals(type, body.type)
+                && Objects.equals(color, body.color);
+    }
+
+    /**
+     * Returns this object's hashcode.
+     *
+     * @return Object's hashcode.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, type, color);
+    }
+
+    /**
+     * Returns current object state as String object.
+     *
+     * @return Current object state as String object.
+     */
+    @Override
+    public String toString() {
+        return String.format(
+                "Body{id=%d, type='%s', color='%s'}",
+                this.id, this.type, this.color);
+    }
+
+    /* * * * * * * * * * * *
+     * getters and setters
+     * * * * * * * * * * * */
+
+    /**
+     * Returns id.
+     *
+     * @return Value of id field.
+     */
+    public int getId() {
+        return this.id;
+    }
+
+    /**
+     * Sets id value.
+     *
+     * @param id Value to set.
+     */
+    public Body setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    /**
+     * Returns type.
+     *
+     * @return Value of type field.
+     */
+    public String getType() {
+        return this.type;
+    }
+
+    /**
+     * Sets type value.
+     *
+     * @param type Value to set.
+     */
+    public Body setType(String type) {
+        this.type = type;
+        return this;
+    }
+
+    /**
+     * Returns color.
+     *
+     * @return Value of color field.
+     */
+    public String getColor() {
+        return this.color;
+    }
+
+    /**
+     * Sets color value.
+     *
+     * @param color Value to set.
+     */
+    public Body setColor(String color) {
+        this.color = color;
+        return this;
+    }
+}
---
+++ src/main/java/carstore/model/xml/Engine.java
@@ -0,0 +1,142 @@
+package carstore.model.xml;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Objects;
+
+/**
+ * Car engine bean.
+ *
+ * @author Aleksei Sapozhnikov (vermucht@gmail.com)
+ * @version 0.1
+ * @since 0.1
+ */
+public class Engine {
+    /**
+     * Logger.
+     */
+    @SuppressWarnings("unused")
+    private static final Logger LOG = LogManager.getLogger(Engine.class);
+
+    /**
+     * Unique id.
+     */
+    private int id;
+
+    /**
+     * Engine type (gasoline, kerosene, gas)
+     */
+    private String engineType;
+    /**
+     * Engine volume, liters (1.2)
+     */
+    private int engineVolume;
+
+    /* * * * * * * * * * * * * * * * * *
+     * equals(), hashCode(), toString()
+     * * * * * * * * * * * * * * * * * */
+
+    /**
+     * Object.equals() method override.
+     *
+     * @param o Other object.
+     * @return <tt>true</tt> if this and given objects are equal, <tt>false</tt> if not.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Engine engine = (Engine) o;
+        return id == engine.id
+                && engineVolume == engine.engineVolume
+                && Objects.equals(engineType, engine.engineType);
+    }
+
+    /**
+     * Returns this object's hashcode.
+     *
+     * @return Object's hashcode.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, engineType, engineVolume);
+    }
+
+    /**
+     * Returns current object state as String object.
+     *
+     * @return Current object state as String object.
+     */
+    @Override
+    public String toString() {
+        return String.format(
+                "Engine{id=%d, engineType='%s', engineVolume=%d}",
+                this.id, this.engineType, this.engineVolume);
+    }
+
+    /* * * * * * * * * * * *
+     * getters and setters
+     * * * * * * * * * * * */
+
+    /**
+     * Returns id.
+     *
+     * @return Value of id field.
+     */
+    public int getId() {
+        return this.id;
+    }
+
+    /**
+     * Sets id value.
+     *
+     * @param id Value to set.
+     */
+    public Engine setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    /**
+     * Returns engineType.
+     *
+     * @return Value of engineType field.
+     */
+    public String getEngineType() {
+        return this.engineType;
+    }
+
+    /**
+     * Sets engineType value.
+     *
+     * @param engineType Value to set.
+     */
+    public Engine setEngineType(String engineType) {
+        this.engineType = engineType;
+        return this;
+    }
+
+    /**
+     * Returns engineVolume.
+     *
+     * @return Value of engineVolume field.
+     */
+    public int getEngineVolume() {
+        return this.engineVolume;
+    }
+
+    /**
+     * Sets engineVolume value.
+     *
+     * @param engineVolume Value to set.
+     */
+    public Engine setEngineVolume(int engineVolume) {
+        this.engineVolume = engineVolume;
+        return this;
+    }
+}
