592f8a1db1e13537c64231fc4064d39cd39a1709
vermucht <vermucht@gmail.com>
2019-03-29 14:32:28 +0300

    FrontItem class in back- and frontend + tests for backend 01:17:23

--- src/main/java/carstore/model/FrontItem.java
+++ src/main/java/carstore/model/FrontItem.java
@@ -1,62 +1,138 @@
 package carstore.model;
 
-import com.google.gson.Gson;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
- * Frontend item object to show on main page.
+ * Frontend item object. Similar object is on the front
+ * side, so it's easy to pass values using JSON.
  *
  * @author Aleksei Sapozhnikov (vermucht@gmail.com)
  * @version 0.1
  * @since 0.1
  */
+@SuppressWarnings({"MismatchedQueryAndUpdateOfCollection", "unused"})
 public class FrontItem {
     /**
      * Logger.
      */
     @SuppressWarnings("unused")
     private static final Logger LOG = LogManager.getLogger(FrontItem.class);
-
+    /**
+     * Locale to format numbers.
+     */
+    private static final Locale LOCALE = new Locale("ru");
+    /**
+     * Item title.
+     */
     private String title;
-
+    /**
+     * Item price.
+     */
     private String price;
-
+    /**
+     * Item seller (user).
+     */
     private String seller;
-
+    /**
+     * Seller contacts.
+     */
     private String contacts;
+    /**
+     * Map of item descriptions: title -> text
+     */
+    private Map<String, String> descriptions = new LinkedHashMap<>();
+    /**
+     * Map of item's image ids (to get them by id from server).
+     */
+    private Set<Long> imageIds = new LinkedHashSet<>();
 
-    private Map<String, String> descriptions = new HashMap<>();
+    private FrontItem() {
+    }
 
-    public static FrontItem of(Car car) {
+    /**
+     * Returns item with formatted titles and descriptions.
+     *
+     * @param car    Car object.
+     * @param images Car Image objects.
+     * @return Frontend Item object to send as JSON.
+     */
+    public static FrontItem of(Car car, Set<Image> images) {
         var item = new FrontItem();
         item.title = car.getManufacturer() + " " + car.getModel();
-        item.price = String.valueOf(car.getPrice());
+        item.price = String.format(LOCALE, "%,d $", car.getPrice());
         item.seller = car.getOwner().getLogin();
-        item.contacts = car.getOwner().getPhone();
+        item.contacts = String.format("tel: %s", car.getOwner().getPhone());
         item.descriptions.put("Body", String.join("; ",
                 car.getBodyType(),
                 car.getColor()));
         item.descriptions.put("Age", String.join("; ",
                 car.getNewness(),
-                car.getYearManufactured() + " y.m.",
-                String.format(new Locale("ru"), "%,d", car.getMileage()) + " km"));
+                String.format("%d y.m.", car.getYearManufactured()),
+                String.format(LOCALE, "%,d km", car.getMileage())));
+        item.descriptions.put("Engine", String.join("; ",
+                car.getEngineFuel(),
+                String.format("%d cm³", car.getEngineVolume())));
+        item.descriptions.put("Transmission", String.join("; ",
+                car.getTransmissionType()));
+        item.imageIds.addAll(images.stream().map(Image::getId).collect(Collectors.toList()));
         return item;
     }
 
-    public static void main(String[] args) {
-        var car = new Car()
-                .setManufacturer("Ford").setModel("Transit FX-300")
-                .setPrice(1500)
-                .setOwner(User.of("Vasily", "12345", "564-345-23"))
-                .setBodyType("Sedan").setColor("black")
-                .setNewness("used").setYearManufactured(2010).setMileage(150_000);
-        var item = FrontItem.of(car);
-        var gson = new Gson().toJson(item);
-        System.out.println(gson);
+    /**
+     * Returns title.
+     *
+     * @return Value of title field.
+     */
+    public String getTitle() {
+        return this.title;
+    }
+
+    /**
+     * Returns price.
+     *
+     * @return Value of price field.
+     */
+    public String getPrice() {
+        return this.price;
+    }
+
+    /**
+     * Returns seller.
+     *
+     * @return Value of seller field.
+     */
+    public String getSeller() {
+        return this.seller;
+    }
+
+    /**
+     * Returns contacts.
+     *
+     * @return Value of contacts field.
+     */
+    public String getContacts() {
+        return this.contacts;
+    }
+
+    /**
+     * Returns descriptions.
+     *
+     * @return Value of descriptions field.
+     */
+    public Map<String, String> getDescriptions() {
+        return new LinkedHashMap<>(this.descriptions);
+    }
+
+    /**
+     * Returns imageIds.
+     *
+     * @return Value of imageIds field.
+     */
+    public Set<Long> getImageIds() {
+        return new LinkedHashSet<>(this.imageIds);
     }
 }
