58a1cd9b0b042ca9477daa9168298090e5474244
vermucht <vermucht@gmail.com>
2019-04-10 14:42:46 +0300

    Hotfix: regexp in validation

--- src/main/java/carstore/factory/CarParamsValidator.java
+++ src/main/java/carstore/factory/CarParamsValidator.java
@@ -21,7 +21,7 @@ public class CarParamsValidator {
     @SuppressWarnings("unused")
     private static final Logger LOG = LogManager.getLogger(CarParamsValidator.class);
     public static final String PTN_MANUFACTURER = "[a-zA-Z][a-zA-Z0-9\\s/-]{2,254}";
-    public static final String PTN_MODEL = "[a-zA-Z][a-zA-Z0-9\\s/-]{2,254}";
+    public static final String PTN_MODEL = "[a-zA-Z0-9][a-zA-Z0-9\\s/-]{2,254}";
     public static final String PTN_COLOR = "[a-zA-Z][a-zA-Z\\s/-]{2,254}";
     public static final String PTN_BODY_TYPE = "[a-zA-Z]{3,255}";
     public static final String PTN_NEWNESS = "(new|used|old)";
@@ -39,7 +39,7 @@ public class CarParamsValidator {
         result = result && intParams.get(Car.IntParam.PRICE) > 0;
         result = result
                 && intParams.get(Car.IntParam.YEAR_MANUFACTURED) > 1900
-                && intParams.get(Car.IntParam.YEAR_MANUFACTURED) < LocalDate.now().getYear();
+                && intParams.get(Car.IntParam.YEAR_MANUFACTURED) <= LocalDate.now().getYear();
         result = result && intParams.get(Car.IntParam.MILEAGE) >= 0;
         result = result && intParams.get(Car.IntParam.ENGINE_VOLUME) > 0;
         return result;
